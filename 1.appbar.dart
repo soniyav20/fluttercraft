f5b96c2dca824091593ed2cfec213a5891e51273
soniyav20 <88935039+soniyav20@users.noreply.github.com>
2024-04-15 20:11:13 +0530

    Update 1.appbar.dart

--- 1.appbar.dart
+++ 1.appbar.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-  home: Home()));
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-      title: Text("Flutter demo"),
-      backgroundColor: Colors.teal,
-      ),
-        body: Center(
-          child: Text('Hello, World!'),
+        title: Text("Profile"),
       ),
     );
   }
